33677f75d0d16273a7ff3ed6ed3f4637d18aee65
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-10 12:13:44 -0400

    Update ones_and_zeros.js

--- ones_and_zeros.js
+++ ones_and_zeros.js
@@ -1,7 +1,6 @@
 /******************************************************************************************
  *                        CODEWARS ONES AND ZEROS CHALLENGE                               *
  *                                                                                        *
- *                                                                                        *
  * Problem Statement                                                                      *
  * Given an array of ones and zeroes, convert the equivalent binary value to an integer.  *
  * Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.           *
