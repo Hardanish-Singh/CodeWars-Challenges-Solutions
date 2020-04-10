5fc62267eb69ba65bd51817cb868dae94c842655
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-10 10:57:30 -0400

    Add files via upload

---
+++ ones_and_zeros.js
@@ -0,0 +1,28 @@
+/******************************************************************************************
+ *                        CODEWARS ONES AND ZEROS CHALLENGE                               *
+ *                                                                                        *
+ *                                                                                        *
+ * Problem Statement                                                                      *
+ * Given an array of ones and zeroes, convert the equivalent binary value to an integer.  *
+ * Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.           *
+ *                                                                                        *
+ * Examples                                                                               *
+ * Input 1: [0, 0, 0, 1]                                                                  *
+ * Output 1: 1                                                                            *
+ *                                                                                        *
+ * Input 2: [1, 0, 0, 1]                                                                  *
+ * Output 2: 9                                                                            *
+ *                                                                                        *
+ * Input 3: [1, 1, 1, 1]                                                                  *
+ * Output 3: 15                                                                           *
+ *****************************************************************************************/
+
+const binaryArrayToNumber = (arr) => {
+  let binarySum = 0;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 1) binarySum += Math.pow(2, arr.length - 1 - i);
+  }
+
+  return binarySum;
+};
