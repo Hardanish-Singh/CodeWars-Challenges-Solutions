60df748cedec07a52e5d2d90e5043651d5f18457
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-10 09:08:09 -0400

    Add files via upload

---
+++ create_phone_numbers.js
@@ -0,0 +1,36 @@
+/******************************************************************************************
+ *                        CODEWARS CREATE PHONE NUMBERS CHALLENGE                         *
+ *                                                                                        *
+ * URL: https://www.codewars.com/kata/525f50e3b73515a6db000b83                            *
+ *                                                                                        *
+ * Problem Statement                                                                      *
+ * Write a function that accepts an array of 10 integers (between 0 and 9), that returns  *
+ * a string of those numbers in the form of a phone number.                               *
+ * The returned format must be correct in order to complete this challenge.               *
+ * Don't forget the space after the closing parentheses!                                  *
+ *                                                                                        *
+ * Examples                                                                               *
+ * Input 1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]                                                *
+ * Output 1: "(123) 456-7890"                                                             *
+ *                                                                                        *
+ * Input 2: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]                                                *
+ * Output 2: "(111) 111-1111"                                                             *
+ *                                                                                        *
+ * Input 3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]                                                *
+ * Output 3: "(123) 456-7890"                                                             *
+ *****************************************************************************************/
+
+function createPhoneNumber(numbers) {
+  let phoneNumber = new String("(");
+
+  for (let i = 0; i < numbers.length; i++) {
+    phoneNumber += numbers[i];
+    if (i === 2) {
+      phoneNumber += ") ";
+    }
+    if (i === 5) {
+      phoneNumber += "-";
+    }
+  }
+  return phoneNumber;
+}
