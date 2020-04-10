6cbc409d1a573f959dc5112866055102cd9d3f3e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-10 09:58:09 -0400

    Update unique_in_order.js

--- unique_in_order.js
+++ unique_in_order.js
@@ -1,7 +1,6 @@
 /******************************************************************************************
  *                        CODEWARS UNIQUE IN ORDER CHALLENGE                              *
  *                                                                                        *
- * URL: https://www.codewars.com/kata/546f922b54af40e1e90001da                            *
  *                                                                                        *
  * Problem Statement                                                                      *
  * Implement the function unique_in_order which takes as argument a sequence and returns a*
