0773fa04136fcd1c3483906e07cd2692ffd0c12e
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-10 12:13:28 -0400

    Update is_this_a_triangle.js

--- is_this_a_triangle.js
+++ is_this_a_triangle.js
@@ -1,7 +1,6 @@
 /******************************************************************************************
  *                        CODEWARS IS THIS A TRIANGLE CHALLENGE                           *
  *                                                                                        *
- *                                                                                        *
  * Problem Statement                                                                      *
  * Implement a method that accepts 3 integer values a, b, c. The method should return true*
  * if a triangle can be built with the sides of given length and false in any other case. *
