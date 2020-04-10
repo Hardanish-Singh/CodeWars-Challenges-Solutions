acca86c1cf59f46e84cd40908e4875c8f88a04a7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-04-10 12:14:01 -0400

    Update replace_with_alphabet_position.js

--- replace_with_alphabet_position.js
+++ replace_with_alphabet_position.js
@@ -1,7 +1,6 @@
 /******************************************************************************************
  *                   CODEWARS REPLACE WITH ALPHABET POSITION CHALLENGE                    *
  *                                                                                        *
- *                                                                                        *
  * Problem Statement                                                                      *
  * In this kata you are required to, given a string, replace every letter with its        *
  * position in the alphabet. If anything in the text isn't a letter, ignore it & don't    *
